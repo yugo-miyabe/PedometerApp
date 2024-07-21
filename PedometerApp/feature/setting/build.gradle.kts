38f2b30f660b4d0e2a3e8acb6aee51ba42d8beb7
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-07-21 22:03:30 +0900

    ビルドの設定修正

--- PedometerApp/feature/setting/build.gradle.kts
+++ PedometerApp/feature/setting/build.gradle.kts
@@ -23,9 +23,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     kotlinOptions {
         jvmTarget = "1.8"
