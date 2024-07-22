fe4e428b01d05a0f08f5d4252dc2fe42337fbd31
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-07-23 00:24:19 +0900

    改行追加

--- PedometerApp/feature/home/src/main/java/jp/yuyuyu/home/HomeScreen.kt
+++ PedometerApp/feature/home/src/main/java/jp/yuyuyu/home/HomeScreen.kt
@@ -1,13 +1,15 @@
 package jp.yuyuyu.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen() {
-    Surface() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column() {
             Text(text = "Home")
             Text(text = "Home")
@@ -15,7 +17,6 @@ fun HomeScreen() {
             Text(text = "Home")
             Text(text = "Home")
             Text(text = "Home")
-
         }
     }
 }
