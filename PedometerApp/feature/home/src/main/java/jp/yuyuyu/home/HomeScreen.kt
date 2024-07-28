3adddbe79f310083d3fc8680def34decf19e681f
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-07-28 11:25:29 +0900

    スペース追加

--- PedometerApp/feature/home/src/main/java/jp/yuyuyu/home/HomeScreen.kt
+++ PedometerApp/feature/home/src/main/java/jp/yuyuyu/home/HomeScreen.kt
@@ -1,7 +1,13 @@
 package jp.yuyuyu.home
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,21 +16,31 @@ import androidx.compose.ui.Modifier
 @Composable
 fun HomeScreen() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column() {
-            Text(text = "Home")
-            Text(text = "Home")
-            Text(text = "Home")
-            Text(text = "Home")
-            Text(text = "Home")
-            Text(text = "Home")
+        val insetsPadding =
+            WindowInsets.systemBars.only(
+                WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom
+            ).asPaddingValues()
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentPadding = insetsPadding
+        ) {
+            item {
+                Text(text = "Home_1")
+            }
+            item {
+                Text(text = "Home_2")
+            }
+            item {
+                Text(text = "Home_3")
+            }
+            item {
+                Text(text = "Home_4")
+            }
+            item {
+                Text(text = "Home_5")
+            }
         }
     }
 }
-
-@Composable
-private fun Home_Preview() {
-
-}
-
-
-
