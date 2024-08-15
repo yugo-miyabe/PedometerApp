c7395612d4d390b35e819bf5ded1dca8f783ec17
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-08-15 15:08:08 +0900

    変更

--- PedometerApp/feature/home/src/main/java/jp/yuyuyu/home/HomeScreen.kt
+++ PedometerApp/feature/home/src/main/java/jp/yuyuyu/home/HomeScreen.kt
@@ -1,27 +1,7 @@
 package jp.yuyuyu.home
 
-import android.Manifest
-import android.util.Log
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.hilt.navigation.compose.hiltViewModel
-import jp.yuyuyu.ui.content.HomeContent
 
 @Composable
-fun HomeScreen(
-    viewModel: HomeViewModel = hiltViewModel()
-) {
-    val permissionLaunch =
-        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { isGranted ->
-            // TODO
-            Log.d("tag", isGranted.toString())
-        }
-
-    HomeContent()
-
-    LaunchedEffect(Unit) {
-        permissionLaunch.launch(Manifest.permission.ACTIVITY_RECOGNITION)
-    }
+fun HomeScreen() {
 }
