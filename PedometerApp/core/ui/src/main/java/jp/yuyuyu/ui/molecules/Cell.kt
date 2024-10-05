784b18ca00a00ab8256024678221b89281403a93
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-10-05 20:03:19 +0900

    カードの項目追加

--- PedometerApp/core/ui/src/main/java/jp/yuyuyu/ui/molecules/Cell.kt
+++ PedometerApp/core/ui/src/main/java/jp/yuyuyu/ui/molecules/Cell.kt
@@ -1,8 +1,12 @@
 package jp.yuyuyu.ui.molecules
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,9 +20,19 @@ import jp.yuyuyu.ui.util.PreviewDynamicTheme
 fun ListItem(
     text: String,
     modifier: Modifier = Modifier,
+    isTop: Boolean = true,
+    isBottom: Boolean = true,
     onClicked: () -> Unit
 ) {
-    Card(onClick = onClicked, modifier = modifier) {
+    Card(
+        onClick = onClicked, modifier = modifier,
+        shape = RoundedCornerShape(
+            topStart = if (isTop) 12.dp else 2.dp,
+            topEnd = if (isTop) 12.dp else 2.dp,
+            bottomEnd = if (isBottom) 12.dp else 2.dp,
+            bottomStart = if (isBottom) 12.dp else 2.dp
+        )
+    ) {
         Box(
             modifier = Modifier
                 .padding(horizontal = 16.dp, vertical = 16.dp)
@@ -34,7 +48,23 @@ fun ListItem(
 private fun ListItemPreview() {
     PedometerAppTheme {
         Surface {
-            ListItem(text = "データとアクセス権限の管理", onClicked = { /* preview */ })
+            Column {
+                ListItem(text = "データとアクセス権限の管理", onClicked = { /* preview */ })
+                Spacer(modifier = Modifier.height(16.dp))
+                ListItem(
+                    text = "利用規約",
+                    isTop = true,
+                    isBottom = false,
+                    onClicked = { /* preview */ }
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                ListItem(
+                    text = "プライパシーポリシー",
+                    isTop = false,
+                    isBottom = true,
+                    onClicked = { /* preview */ }
+                )
+            }
         }
     }
 }
--- PedometerApp/core/ui/src/main/java/jp/yuyuyu/ui/util/extension/ModifierExtension.kt
+++ PedometerApp/core/ui/src/main/java/jp/yuyuyu/ui/util/extension/ModifierExtension.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
-
 fun Modifier.cardDecoration(
     isTop: Boolean,
     isBottom: Boolean,
