b55f352ea0568e1724162fc553a161e5dd50fefb
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-10-11 23:55:12 +0900

    NavHostを追加

--- PedometerApp/feature/setting/src/main/java/jp/yuyuyu/setting/navigation/SettingNavigation.kt
+++ PedometerApp/feature/setting/src/main/java/jp/yuyuyu/setting/navigation/SettingNavigation.kt
@@ -1,8 +1,11 @@
 package jp.yuyuyu.setting.navigation
 
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import jp.yuyuyu.setting.SettingScreen
 import kotlinx.serialization.Serializable
@@ -17,5 +20,14 @@ fun NavController.navigateToSettingScreen(navOptions: NavOptions) {
 fun NavGraphBuilder.settingGraph() {
     composable<SettingRoute> {
         SettingScreen()
+    }
+}
+
+@Composable
+fun SettingNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = SettingRoute) {
+        composable<SettingRoute> {
+            SettingScreen()
+        }
     }
 }
