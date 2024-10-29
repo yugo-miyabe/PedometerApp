f43e2caa75c1b202dfea58a94ebc213b4752fd1f
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-10-29 22:59:28 +0900

    依存関係を追加

--- PedometerApp/core/datasource/build.gradle.kts
+++ PedometerApp/core/datasource/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -40,4 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
 }
