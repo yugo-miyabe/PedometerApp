47d090fd6d4d45afc7cb96b86987a24cdd0535b4
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-11-22 19:48:37 +0900

    トップモジュールにtakahiromの依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.detekt)
 }
 
