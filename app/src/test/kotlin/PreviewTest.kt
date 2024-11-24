fedb6ffb0cb315a46f12b306314dff8ddd4b63b5
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-11-25 01:01:11 +0900

    スクリーンショットを実行できるよう修正

--- app/src/test/kotlin/PreviewTest.kt
+++ app/src/test/kotlin/PreviewTest.kt
@@ -1,7 +1,10 @@
 import com.airbnb.android.showkase.models.Showkase
 import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
 import com.github.takahirom.roborazzi.DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH
+import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziOptions
+import com.github.takahirom.roborazzi.RoborazziTaskType
 import com.github.takahirom.roborazzi.captureRoboImage
 import jp.yuyuyu.pedometerapp.getMetadata
 import org.junit.Test
@@ -12,24 +15,28 @@ import org.robolectric.annotation.GraphicsMode
 
 @RunWith(ParameterizedRobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
-@Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel5)
 class PreviewTest(
     private val showkaseBrowserComponent: ShowkaseBrowserComponent,
 ) {
+    @OptIn(ExperimentalRoborazziApi::class)
     @Test
     fun previewScreenshot() {
+        val roborazziOptions = RoborazziOptions(
+            taskType = RoborazziTaskType.Record
+        )
         val filePath =
             DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH + "/" + showkaseBrowserComponent.componentKey + ".png"
         captureRoboImage(
-            filePath,
-        ) {
-            // ここが実行されてない
-            showkaseBrowserComponent.component()
-        }
+            filePath = filePath,
+            roborazziOptions = roborazziOptions,
+            content = {
+                showkaseBrowserComponent.component()
+            }
+        )
     }
 
     companion object {
-
         @ParameterizedRobolectricTestRunner.Parameters
         @JvmStatic
         fun components(): Iterable<Array<Any?>> {
