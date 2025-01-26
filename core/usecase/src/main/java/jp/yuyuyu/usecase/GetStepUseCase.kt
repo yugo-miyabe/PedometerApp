eb93244408f35698207ef2417fe53693ac8e0dd7
yugo-miyabe <yuni42yuyuyu@gmail.com>
2025-01-26 10:13:31 +0900

    DBの取得処理を追加

--- core/usecase/src/main/java/jp/yuyuyu/usecase/GetStepUseCase.kt
+++ core/usecase/src/main/java/jp/yuyuyu/usecase/GetStepUseCase.kt
@@ -2,9 +2,10 @@ package jp.yuyuyu.usecase
 
 import jp.yuyuyu.repository.StepRepository
 import javax.inject.Inject
+import jp.yuyuyu.model.StepModel
 
 class GetStepUseCase @Inject constructor(
     private val stepRepository: StepRepository,
 ) {
-    suspend operator fun invoke(date: String) = stepRepository.getStepCount(date = date)
+    suspend operator fun invoke(date: String): StepModel = stepRepository.getStepCount(date = date)
 }
